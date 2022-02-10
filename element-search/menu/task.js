7dbdf037117d1d2a9c05ff68a3c4a9b8445c7840
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-02-10 14:59:13 +0300

    himework v3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,12 @@
 let link = Array.from(document.querySelectorAll('.menu__link'));
-// let itemMenu = Array.from(document.querySelectorAll('.menu__item'));
-// let subMenu = document.querySelectorAll('.menu_sub')
 
     for (let i = 0; i < link.length; i++) {
         let x = link[i].parentElement.querySelector('.menu_sub')
         console.log(x)
         if (x !== null) {
-            link[i].addEventListener('click', () => {
-                x.classList.add('menu_active');
+            link[i].addEventListener('click', (event) => {
+                x.classList.toggle('menu_active');
+                event.preventDefault()
             }, false)
         }
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,30 +3,21 @@ let prev = document.querySelector('.slider__arrow_prev');
 let next = document.querySelector('.slider__arrow_next');
 let current = 0;
 
+
 function slide () {
-    for (let i = 0; i < slides.length; i++) {
-         slides[i].classList.remove('slider__item_active');
-    }
+  let x =  Array.from(slides).findIndex( (elem) => {
+        return elem.classList.contains('slider__item_active')
+    })
+    slides[x].classList.remove('slider__item_active')
     slides[current].classList.add('slider__item_active')
 }
 prev.addEventListener('click', () => {
-    if (current -1 == -1) {
-        current = slides.length - 1;
-    } else {
-        current--
-    }
+    current -1 == -1 ? current = slides.length - 1 : current--
     slide(current)
 })
 
 next.addEventListener('click', () => {
-
-    if (current +1 == slides.length) {
-        current = 0;
-
-    } else {
-        current++
-    }
-    console.log(current)
+    current +1 == slides.length ? current = 0 : current++
     slide(current)
 })
 
