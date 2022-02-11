bb2ee499c470eedf98115dd17ad1874d6537c425
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-02-11 12:42:55 +0300

    tasks 1, 3

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,48 +1,50 @@
 let button = Array.from(document.querySelectorAll('.font-size'));
-let small = document.querySelector('.font-size_small')
-let big = document.querySelector('.font-size_big')
-let book = document.querySelector('.book')
+let content = document.querySelector('.book__content')
 
-// button.forEach((element) => {
-//     element.addEventListener('click', (el) => {
-//         let x =  el.findIndex( (elem) => {
-//             return elem.classList.contains('font-size_active')
-//         })
-//         if (x) {
-//             element.classList.remove('font-size_active')
-//         } else {
-//             element.classList.add('font-size_active');
-//             el.preventDefault();
-//         }
+button.forEach((element) => {
+
+    element.addEventListener('click', (el) => {
+        let current = document.querySelector('.font-size_active')
+        el.preventDefault()
+        current.classList.remove('font-size_active')
+        element.classList.add('font-size_active')
+let x = element.getAttribute('data-size')
+        console.log( x)
+        if (x === 'small') {
+            content.classList.add('book_fs-small')
+        }
+        if (x === 'big') {
+            content.classList.add('book_fs-big')
+        }
+    })
+
+})
+
+// function change () {
+//     let x =  button.findIndex( (elem) => {
+//         return elem.classList.contains('font-size_active')
 //     })
+//     button[x].classList.remove('font-size_active')
+//     button.classList.toggle('font-size_active')
+//
+// }
+//
+// small.addEventListener('click', (elem) => {
+//     small.classList.add('font-size_active')
+//     book.classList.add('book_fs-small')
+//     elem.preventDefault()
+//     change()
+//
+//
 //
 // })
-
-function change () {
-    let x =  button.findIndex( (elem) => {
-        return elem.classList.contains('font-size_active')
-    })
-    button[x].classList.remove('font-size_active')
-    button.classList.toggle('font-size_active')
-
-}
-
-small.addEventListener('click', (elem) => {
-    small.classList.add('font-size_active')
-    book.classList.add('book_fs-small')
-    elem.preventDefault()
-    change()
-
-
-
-})
-
-big.addEventListener('click', (elem) => {
-    big.classList.add('font-size_active')
-    book.classList.add('book_fs-big')
-    elem.preventDefault()
-    change()
-
-
-
-})
+//
+// big.addEventListener('click', (elem) => {
+//     big.classList.add('font-size_active')
+//     book.classList.add('book_fs-big')
+//     elem.preventDefault()
+//     change()
+//
+//
+//
+// })
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,14 +1,13 @@
 let block = Array.from(document.querySelectorAll('.reveal'));
 
-function isVisible(element) {
-block.forEach((el) => {
-    const {top, bottom} = el.getBoundingClientRect();
-    if (bottom < 0 || top > window.innerHeight) {
-        return false
-    }
-    el.classList.add('reveal_active')
-    return true
-})
-}
 
-isVisible(block)
+window.addEventListener('scroll', () => {
+    block.forEach((el) => {
+        const {top, bottom} = el.getBoundingClientRect();
+        if (bottom < 0 || top > window.innerHeight) {
+            return false
+        }
+        el.classList.add('reveal_active')
+        return true
+    })
+} )
