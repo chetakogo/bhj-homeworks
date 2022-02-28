2bf46074ab835e18cd968c64c882a91bd4c0e645
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-02-28 12:11:14 +0300

    tasks

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,21 +1,25 @@
 const xhr = new XMLHttpRequest;
-let poll = document.querySelector('.poll');
-let pollTitle = document.querySelector('#poll__title');
-let pollAnswers = document.querySelector('#poll__answers')
+const poll = document.querySelector('.poll');
+const pollTitle = document.querySelector('#poll__title');
+const pollAnswers = document.querySelector('#poll__answers')
 
 xhr.open( 'GET', 'https://netology-slow-rest.herokuapp.com/poll.php' );
 xhr.send()
 
-xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
+xhr.addEventListener('load', () => {
+    if (xhr.readyState === xhr.DONE && xhr.status == 200 ) {
         let formatedData = JSON.parse(xhr.responseText);
         console.log(formatedData)
         pollTitle.textContent = formatedData.data.title
         for (let i in formatedData.data.answers) {
-            pollAnswers.insertAdjacentHTML("afterbegin",
-                `<button class="poll__answer">${formatedData.data.answers[i]}</button>`)
+            // pollAnswers.insertAdjacentHTML("afterbegin",
+            //     `<button class="poll__answer">${formatedData.data.answers[i]}</button>`)
+            const pollAnswer = document.createElement('button')
+            pollAnswer.classList.add('poll__answer');
+            pollAnswer.textContent = formatedData.data.answers[i];
+            pollAnswers.append(pollAnswer)
         }
-        let button = document.querySelectorAll('.poll__answer')
+        const button = document.querySelectorAll('.poll__answer')
         for (let elem of button) {
             elem.addEventListener('click', (event) => {
                 alert("Спасибо, Ваш голос засчитан")
--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,22 +1,37 @@
 const xhr = new XMLHttpRequest;
 const loader = document.querySelector('#loader')
-let item = document.querySelector('.item')
-let items = document.querySelector('#items')
+const item = document.querySelector('#items')
 
 xhr.open( 'GET', 'https://netology-slow-rest.herokuapp.com' );
 xhr.send()
 
 
 
-xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
+xhr.addEventListener('load', () => {
+    if (xhr.readyState === xhr.DONE && xhr.status == 200) {
         loader.classList.remove('loader_active');
         let formatedData = JSON.parse(xhr.responseText)
+
         for (let i in formatedData.response.Valute) {
-            item.insertAdjacentHTML("beforebegin", `
-        <div class="item__code">${formatedData.response.Valute[i].CharCode}</div>
-        <div class="item__value">${formatedData.response.Valute[i].Value}</div>
-        <div class="item__currency">руб.</div>`)
+        //     item.insertAdjacentHTML("beforebegin", `
+        // <div class="item__code">${formatedData.response.Valute[i].CharCode}</div>
+        // <div class="item__value">${formatedData.response.Valute[i].Value}</div>
+        // <div class="item__currency">руб.</div>`)
+            const code = document.createElement('div')
+            code.classList.add('item__code');
+            code.textContent = formatedData.response.Valute[i].CharCode
+
+            const itemValue = document.createElement('div')
+            itemValue.classList.add('item__value');
+            itemValue.textContent = formatedData.response.Valute[i].Value
+
+            const itemCurrency = document.createElement('div')
+            itemCurrency.classList.add('item__value');
+            itemCurrency.textContent = 'руб.'
+
+            item.append(code)
+            item.append(itemValue)
+            item.append(itemCurrency)
         }
 
     }
